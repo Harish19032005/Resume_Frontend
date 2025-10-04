1eb478a0095228d04202107cd6bc2939eadb1912
Harish19032005 <hh5482481@gmail.com>
2025-10-05 00:54:21 +0530

    Commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://resume-backend.onrender.com/api',
   timeout: 5000
 });
 
