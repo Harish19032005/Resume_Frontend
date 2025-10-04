b59bdd286cceb5eafc3c1fb505fb18d9551f069f
Harish19032005 <hh5482481@gmail.com>
2025-10-05 01:47:39 +0530

    Added Backend API Ready

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://resume-backend.onrender.com/api',
+  baseURL: 'https://resume-backend-a3dw.onrender.com/',
   timeout: 5000
 });
 
